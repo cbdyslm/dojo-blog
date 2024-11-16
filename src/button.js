13facb66d4580355267a9091a9dbe1315b4808e7
cbdyslm8 <ayusufm@outlook.com>
2024-11-16 12:38:43 +0300

    more work

---
+++ src/button.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Card } from '@mui/material'
+
+function button() {
+    return (
+        <div className="secondbox">
+         <Card sx={{ border: '1px solid #ccc' }}>
+  <h2>Yellow</h2>
+</Card>
+        </div>
+        
+    )
+}
+
+export default button
---
+++ src/hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className="hero">
+      
+        
+    <h2>Header</h2>
+  
+</div>
+  );
+}
+
+export default Hero;
